5f012e443ee9a633cd571de797540ee4c8c75f1b
Brian Lach <brianlach72@gmail.com>
2022-11-28 00:42:30 -0500

    add animated main_entrance_door model

--- src/props_well/Sources.pp
+++ src/props_well/Sources.pp
@@ -1,6 +1,13 @@
 #define DIR_TYPE models
 #define INSTALL_TO models/props_well
 
+// Animated sliding garage door type thing (used in Well and Lumberyard).
+#call tf_char_egg main_entrance_door,idle close open,
+
+#begin install_mdl
+  #define SOURCES main_entrance_door.pmdl
+#end install_mdl
+
 #include $[THISDIRPREFIX]Sources.hand_truck01.pp
 #include $[THISDIRPREFIX]Sources.computer_cart01.pp
 #include $[THISDIRPREFIX]Sources.main_entrance_door_frame.pp
