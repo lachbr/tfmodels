e2d34bbc73ca6e7b42945de3b9e1d7295cffe829
Brian Lach <brianlach72@gmail.com>
2022-12-16 02:47:32 -0500

    shell models

--- src/weapons/shells/Sources.pp
+++ src/weapons/shells/Sources.pp
@@ -1,25 +1,14 @@
 #define DIR_TYPE models
 #define INSTALL_TO models/weapons
 
-#begin install_tex
-  #define INSTALL_TO maps
-  #define SOURCES \
-    all_shells.ptex \
-    all_shells_normal.ptex
-#end install_tex
-
-#begin install_mat
-  #define INSTALL_TO materials
-  #define SOURCES \
-    all_shells.pmat
-#end install_mat
-
 #begin blender_egg
   #define SOURCES \
-    shell_shotgun.blend
+    shell_shotgun.blend shell_pistol.blend shell_sniperrifle.blend
 #end blender_egg
 
-#begin install_egg
-  #define UNPAL_SOURCES \
-    shell_shotgun.egg
-#end install_egg
+#begin install_mdl
+  #define SOURCES \
+    shell_shotgun.pmdl \
+    shell_pistol.pmdl \
+    shell_sniperrifle.pmdl
+#end install_mdl
