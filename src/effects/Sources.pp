250bdab3b51ad05974e7377a6b515e27567131cc
Brian Lach <brianlach72@gmail.com>
2023-01-05 23:53:42 -0500

    cappoint hologram

--- src/effects/Sources.pp
+++ src/effects/Sources.pp
@@ -6,10 +6,15 @@
     explosion.egg
 #end install_egg
 
+#begin blender_egg
+  #define SOURCES cappoint_hologram.blend
+#end blender_egg
+
 #call tf_char_egg minigunmuzzle,idle,
 
 #begin install_mdl
   #define SOURCES \
-    minigunmuzzle.pmdl
+    minigunmuzzle.pmdl \
+    cappoint_hologram.pmdl
 
 #end install_mdl
